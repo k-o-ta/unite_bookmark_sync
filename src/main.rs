590e7d9227cea9dfa74c00cb9e37845e3a3474df
Koji Ota <afterjnih@gmail.com>
2017-03-12 17:25:42 +0900

    save to shared directory

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ extern crate yaml_rust;
 use yaml_rust::{yaml, Yaml};
 use std::env;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::Read;
 use std::path::Path;
+use std::io::BufReader;
+use std::io::BufRead;
 
 #[derive(Debug)]
 struct Bookmark<'a> {
     local_bookmark_path: &'a Path,
     shared_bookmark_path: &'a Path,
-    // local_project_path: &'a Path,
     projects: Vec<Project<'a>>,
 }
 
@@ -88,9 +90,39 @@ fn load_from_file() -> Result<Vec<Yaml>, String> {
 
 fn sync_bookmarks(bookmark: Bookmark) {
     for project in bookmark.projects {
-        let file_name = format!("{}/{}",
-                                bookmark.shared_bookmark_path.to_str().unwrap(),
-                                project.name);
-        File::create(file_name);
+        let shared_file_name = format!("{}/{}",
+                                       bookmark.shared_bookmark_path.to_str().unwrap(),
+                                       project.name);
+        let local_file_name = format!("{}/{}",
+                                      bookmark.local_bookmark_path.to_str().unwrap(),
+                                      project.name);
+        let mut shared_file = File::create(shared_file_name).unwrap();
+        match File::open(local_file_name) {
+            Ok(f) => {
+                let mut file = BufReader::new(&f);
+                let mut lines = file.lines();
+                lines.next();
+                shared_file.write_fmt(format_args!("0.1.0\n"));
+                for line in lines {
+                    match line {
+                        Ok(l) => {
+                            let bookmark_info: Vec<&str> = l.split('\t').collect();
+                            let bookmarked_file = bookmark_info[1]
+                                .to_string()
+                                .replacen(project.directory.to_str().unwrap(), "", 1);
+                            shared_file.write_fmt(format_args!("{}\t{}\t{}\t{}\n",
+                                                               bookmark_info[0],
+                                                               bookmarked_file,
+                                                               bookmark_info[2],
+                                                               bookmark_info[3]));
+                        }
+                        Err(_) => {}
+                    }
+                }
+            }
+            Err(_) => {
+                continue;
+            }
+        }
     }
 }
